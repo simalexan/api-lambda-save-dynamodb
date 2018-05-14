593f0427066b126b2cee9c027a605867b59f315a
simalexan <alexander.simovic@gmail.com>
2018-05-14 02:18:15 +0200

    fixes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const AWS = require('aws-sdk');
-const dynamoDb = new AWS.DnyamoDB.DocumentClient()
-const processResponse = require('./process-response');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuidv4 = require('uuid/v4');
+const processResponse = require('./process-response.js');
 const TABLE_NAME = process.env.TABLE_NAME;
 const IS_CORS = process.env.IS_CORS;
+const PRIMARY_KEY = process.env.PRIMARY_KEY;
 
 exports.handler = (event) => {
     if (event.httpMethod === 'OPTIONS') {
@@ -11,13 +13,15 @@ exports.handler = (event) => {
     if (!event.body) {
         return Promise.resolve(processResponse(IS_CORS, 'invalid', 400));
     }
+    let item = JSON.parse(event.body);
+    item[PRIMARY_KEY] = uuidv4();
     let params = {
         TableName: TABLE_NAME,
-        Item: event.body
+        Item: item
     }
     return dynamoDb.put(params)
     .promise()
-    .then(() => (processResponse(IS_CORS, '', 400)))
+    .then(() => (processResponse(IS_CORS)))
     .catch(err => {
         console.log(err);
         return processResponse(IS_CORS, 'dynamo-error', 500);
