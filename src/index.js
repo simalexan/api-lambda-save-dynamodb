8cd2a473984a3ee0f3b5052044050a483736439e
Nsupyq <93358967+Nsupyq@users.noreply.github.com>
2021-10-31 15:08:07 +0800

    add idempotence check in index.js

--- src/index.js
+++ src/index.js
@@ -14,20 +14,44 @@ exports.handler = async event => {
     return processResponse(IS_CORS, 'invalid', 400);
   }
   const item = JSON.parse(event.body);
-  item[PRIMARY_KEY] = uuidv4();
-  const params = {
-    TableName: TABLE_NAME,
-    Item: item
-  }
+  
+  //require user to provide an unique primary key
+  if(!item[PRIMARY_KEY])
+    return processResponse(IS_CORS, 'please provide unique primary key', 400);
+  
+  // idempotence check
   try {
-    await dynamoDb.put(params).promise()
-    return processResponse(IS_CORS);
-  } catch (error) {
-    let errorResponse = `Error: Execution update, caused a Dynamodb error, please look at your logs.`;
-    if (error.code === 'ValidationException') {
-      if (error.message.includes('reserved keyword')) errorResponse = `Error: You're using AWS reserved keywords as attributes`;
+    var params1 = {
+      TableName: TABLE_NAME,
+      Key:{
+          'savetableId': item[PRIMARY_KEY]
+      }
+    };
+    var data = await dynamoDb.get(params1).promise();
+    
+    if(data.Item) {
+      // data have been saved, this is a duplicate write
+      return processResponse(IS_CORS);
     }
-    console.log(error);
-    return processResponse(IS_CORS, errorResponse, 500);
+    // data have not been saved
+    const params = {
+      TableName: TABLE_NAME,
+      Item: item
+    }
+    try {
+      await dynamoDb.put(params).promise()
+      return processResponse(IS_CORS);
+    } catch (error) {
+      let errorResponse = `Error: Execution update, caused a Dynamodb error, please look at your logs.`;
+      if (error.code === 'ValidationException') {
+        if (error.message.includes('reserved keyword')) errorResponse = `Error: You're using AWS reserved keywords as attributes`;
+      }
+      console.log(error);
+      return processResponse(IS_CORS, errorResponse, 500);
+    }
+    
+  } catch (error) {
+    return processResponse(IS_CORS, 'error', 500);
   }
+
 };
