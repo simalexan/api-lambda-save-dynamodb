c5cd1ed24828ae7f5cc017ed7e8be18e0ec48790
Nsupyq <93358967+Nsupyq@users.noreply.github.com>
2021-11-03 22:42:24 +0800

    Remove unnecessary check in index.js
    It is not necessary to check whether the data have been saved. We just need to ensure that the function will create only one record even under retry.

--- src/index.js
+++ src/index.js
@@ -19,39 +19,20 @@ exports.handler = async event => {
   if(!item[PRIMARY_KEY])
     return processResponse(IS_CORS, 'please provide unique primary key', 400);
   
-  // idempotence check
+  const params = {
+    TableName: TABLE_NAME,
+    Item: item
+  }
   try {
-    var params1 = {
-      TableName: TABLE_NAME,
-      Key:{
-          'savetableId': item[PRIMARY_KEY]
-      }
-    };
-    var data = await dynamoDb.get(params1).promise();
-    
-    if(data.Item) {
-      // data have been saved, this is a duplicate write
-      return processResponse(IS_CORS);
-    }
-    // data have not been saved
-    const params = {
-      TableName: TABLE_NAME,
-      Item: item
-    }
-    try {
-      await dynamoDb.put(params).promise()
-      return processResponse(IS_CORS);
-    } catch (error) {
-      let errorResponse = `Error: Execution update, caused a Dynamodb error, please look at your logs.`;
-      if (error.code === 'ValidationException') {
-        if (error.message.includes('reserved keyword')) errorResponse = `Error: You're using AWS reserved keywords as attributes`;
-      }
-      console.log(error);
-      return processResponse(IS_CORS, errorResponse, 500);
-    }
-    
+    await dynamoDb.put(params).promise()
+    return processResponse(IS_CORS);
   } catch (error) {
-    return processResponse(IS_CORS, 'error', 500);
+    let errorResponse = `Error: Execution update, caused a Dynamodb error, please look at your logs.`;
+    if (error.code === 'ValidationException') {
+      if (error.message.includes('reserved keyword')) errorResponse = `Error: You're using AWS reserved keywords as attributes`;
+    }
+    console.log(error);
+    return processResponse(IS_CORS, errorResponse, 500);
   }
 
 };
